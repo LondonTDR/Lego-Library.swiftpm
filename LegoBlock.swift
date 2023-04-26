35d2a9482ac69051431af61453e5fe0bd0d43fa6
Lincoln Drake <ldrake5422@stu.d214.org>
2023-04-26 14:55:02 -0500

    Started Working on Inventory (NOT COMPLETED)

---
+++ LegoBlock.swift
@@ -0,0 +1,16 @@
+//
+//  LegoBlock.swift
+//  Lego_Library
+//
+//  Created by Lincoln Drake on 4/26/23.
+//
+
+import SwiftUI
+
+struct LegoBlock: Hashable {
+    
+    var blockType: String
+    
+    var numberOfBlocks: Int
+    
+}
--- lnventoryView.swift
+++ lnventoryView.swift
@@ -9,6 +9,43 @@ import SwiftUI
 
 struct InventoryView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        @Binding
+        
+        HStack {
+            Text("Inventory")
+                .foregroundColor(.red)
+                .font(.system(size: 30))
+                .frame(width: 350, height: 100, alignment: .center)
+            
+            Button("+") {
+            
+                NavigationLink()  {
+                    AddView()
+                    
+                    
+                }
+                
+                
+                
+            }
+        .foregroundColor(.red)
+        .font(.system(size: 30))
+        .frame(width: 250, height: 100, alignment: .center)
+    
+            
+            List(All, id: \.self) { currentLegoBlock in
+                
+                ListView(currentLegoBlock: currentLegoBlock)
+                    .font(.system(size: 30))
+                    .bold()
+                    .background(.gray)
+                
+                
+                
+                
+            }
+        }
+        
     }
 }
