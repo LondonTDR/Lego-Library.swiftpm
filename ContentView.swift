40a6499bc4d961ec6ff341ba30a0b11c2cabf26e
London <londonsmith@stu.d214.org>
2023-04-26 14:12:26 -0500

    fixed start page

--- ContentView.swift
+++ ContentView.swift
@@ -3,13 +3,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationView {
-            Text("Lego Library")
-                .font(.largeTitle)
-                .foregroundColor(.red)
-                .padding()
             NavigationLink("Start"){
                 InventoryView()
             }
+            .navigationTitle("Lego Library")
+            .font(.largeTitle)
+            .foregroundColor(.red)
+            .padding()
             Text("Start")
                 .foregroundColor(.blue)
                 .padding()
