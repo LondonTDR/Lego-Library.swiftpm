fb24c8b42d044398c2fb9acf5b6f9990b7c171fe
London <londonsmith@stu.d214.org>
2023-05-08 14:02:28 -0500

    I made serach section shell

--- SearchView.swift
+++ SearchView.swift
@@ -8,7 +8,41 @@
 import SwiftUI
 
 struct SearchView: View {
+    // https://legouniverse.fandom.com/wiki/List_of_Bricks
+    let bricks=["Brick 2x4","Brick 2x3","Brick 2x2"]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            List{
+                ForEach(0..<bricks.count,id:\.self){index in
+                    Text(bricks[index])
+                }
+            }
+        }
+        .toolbar {
+            ToolbarItem(placement: ToolbarItemPlacement.bottomBar) {
+                NavigationLink {
+                    InventoryView()
+                } label: {
+                    Image(systemName: "bag")
+                }
+                
+            }
+            ToolbarItem(placement: ToolbarItemPlacement.bottomBar){
+                Button {
+                    
+                } label: {
+                    Image(systemName: "magnifyingglass.circle.fill")
+                }
+                Spacer()
+            }
+            ToolbarItem(placement: ToolbarItemPlacement.bottomBar) {
+                NavigationLink {
+                    LibraryView()
+                } label: {
+                    Image(systemName: "books.vertical")
+                }
+                
+            }
+        }
     }
 }
