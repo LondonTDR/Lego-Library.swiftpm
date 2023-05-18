39b9920876eb3916180fbd22c88dcc0f02051ddb
jhhsxcode <jhhs.xcode@d214.org>
2023-05-18 17:02:57 -0500

    updated

--- ListView.swift
+++ ListView.swift
@@ -10,12 +10,12 @@ struct ListView: View {
             
             Text(currentLegoAmount.Lego).font(.system(size: 25))
             
-            Text("\(currentLegoAmount.Amount)").font(.system(size: 15 ))
+            Text("\(currentLegoAmount.Amount!)").font(.system(size: 15))
             
             
             
             
-        } 
+        }
         .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
         
         
